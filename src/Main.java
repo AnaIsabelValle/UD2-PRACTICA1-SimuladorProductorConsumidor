e6731f85341c5004df65b913a5cefd003479a94e
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-11-15 16:31:49 +0000

    fix: Modificado el Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ import net.salesianos.Verdura;
 public class Main {
     public static void main(String[] args) {
 
-        Restaurante restaurante1 = new Restaurante(20);
+        Restaurante restaurante1 = new Restaurante(5);
 
 
-        Granjero granjero1 = new Granjero("Ana", 10, restaurante1);
-        Granjero granjero2 = new Granjero("Isabel", 10, restaurante1);
+        Granjero granjero1 = new Granjero("Ana", 5, restaurante1);
 
-        Cliente cliente1 = new Cliente("Alba", 20, restaurante1);
+
+        Cliente cliente1 = new Cliente("Alba", 5, restaurante1);
 
         granjero1.start();
-        granjero2.start();
+
 
         cliente1.start();
     }
